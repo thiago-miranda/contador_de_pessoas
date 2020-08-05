21c686d406f546971b25b9c96864dd4d30c7836c
thiago-miranda <thiago.miranda@hotmail.com>
2020-08-05 17:42:09 -0300

    org

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,21 @@ void main() {
           "Pessoas: 0",
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Text("+1",
+                  style: TextStyle(color: Colors.white, fontSize: 40.0)),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text("-1",
+                  style: TextStyle(color: Colors.white, fontSize: 40.0)),
+            ),
+          ],
+        ),
         Text(
           "Pode Entrar",
           style: TextStyle(
