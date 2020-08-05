97195d6edd4d6a8692dc2ff5b2fa1bae34a402db
thiago-miranda <thiago.miranda@hotmail.com>
2020-08-05 18:29:00 -0300

    addImages

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Stack(
       children: <Widget>[
-        Text(
-          "Pessoas: 0",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset(
+          "./images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        Row(
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              onPressed: () {},
-              child: Text("+1",
-                  style: TextStyle(color: Colors.white, fontSize: 40.0)),
+            Text(
+              "Pessoas: 0",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             ),
-            FlatButton(
-              onPressed: () {},
-              child: Text("-1",
-                  style: TextStyle(color: Colors.white, fontSize: 40.0)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("+1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("-1",
+                        style: TextStyle(color: Colors.white, fontSize: 40.0)),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 30),
             ),
           ],
-        ),
-        Text(
-          "Pode Entrar",
-          style: TextStyle(
-              color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
         ),
       ],
     ),
